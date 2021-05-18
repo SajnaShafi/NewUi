692480fa4f1f5aa46d2c78e06430f84fd5c14700
sulthana-parveen <76679665+sulthana-parveen@users.noreply.github.com>
2021-05-18 16:32:34 -0700

    sss

--- scrns/home.js
+++ scrns/home.js
@@ -1,110 +1,107 @@
-import React, {Component,useState} from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Platform, StyleSheet, Text, TextInput, View, Dimensions,TouchableOpacity,
-Button,Alert,Image,ImageBackground,StatusBar} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React,{useEffect,useState} from 'react';
+import { StyleSheet, Text, View,Image,FlatList,Alert} from 'react-native';
+import {Card, Button} from 'react-native-paper';
 
-const Home = ({navigation})=>{
+const Home = (props,{navigation}) => {
 
-    const [email,setEMAIL] = useState("")
-    const [password,setPASSWORD] = useState("")
-   const Signin = ()=>{
-    fetch("http://192.168.42.252:4000/api/user/login",{
-      method:"post",
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify({
-        
-        
-          email,
-          password
-      })
-  })
- // .then(res=>res.json())
-  //.then(async (data)=>{
-    .then(async (res) => {
-    if (res.status == 202) {
-      const cookie = res.headers.get('auth-tocken');
-      await AsyncStorage.setItem('tocken', cookie);
-     // setLoading(false);
-     Alert.alert('login success');
-     navigation.navigate('Create');
-    //   props.navigation.replace('Create');
-    } else if (res.status == 400) {
-     // setLoading(false);
-      Alert.alert('No user found');
-      
-    } else if (res.status == 401) {
-      //setLoading(false);
-      Alert.alert('passwod doesnot match');
-    }
-
-    else if (res.status == 402) {
-      //setLoading(false);
-      Alert.alert('Enter Valid Email/password');
-    }
+     const [data,setData] = useState([])
+     const [loading,setLoading]= useState(true)
     
-})
-   }
 
-    return(
+   // console.log(data,loading)
+   
+     const fetchData = ()=>{
+        fetch("http://172.20.10.2:5000/posts")
+        .then(res=>res.json())
+        .then(results=>{
+    
+             setData(results)
+             setLoading(false)
+    
 
+        }).catch(err=>{
+            Alert.alert("someting went wrong")
+        })
+     }
+    
+     useEffect(()=>{
+          fetchData()
+     },[])
+    
+    const renderList = ((item)=>{
+          return(
+            
+            
+            <Card style={styles.mycard}
+            onPress={()=>props.navigation.navigate('Profile',{item})}
+            >
+                <View style={{marginLeft:10}}>
+                    <Text style={styles.text}>{item.Name}</Text>   
+                       
+                </View>
+                
+           </Card>
+            
+          )
+    })
+   return(
+       <View style={{flex:1}}>
+         
+    
+        <FlatList
+              data={data}
+              renderItem={({item})=>{
+                return renderList(item)
+              }}
+              keyExtractor={item=>item._id}
+              onRefresh={()=>fetchData()}
+              refreshing={loading}
+              />
         
+        
+           
+          
+       </View>
+    
+   ) 
+            }
+    
+const theme = {
+    colors:{
+        primary:"#6495ed",
 
-    <View style={styles.container}>
-         <Text style={{paddingBottom:30,paddingLeft:30,fontSize:25,color:'blue'}}>WELCOME TO LIBRARY</Text>
-
-   <View style={styles.inputContainer}>
-    <TextInput 
-     placeholder="EMAIL"
-     keyboardType="email-address"
-     underlineColorAndroid='transparent'
-     onChangeText={text => setEMAIL(text)}/>
-   </View>
-
-   <View style={styles.inputContainer}>
-    <TextInput 
-     placeholder="PASSWORD"
-     keyboardType="email-address"
-     underlineColorAndroid='transparent'
-     onChangeText={text => setPASSWORD(text)}/>
-   </View>
-
-    <Button   
-    title= "login"
-    color= "blue"
-    onPress={() => Signin()}>
-    </Button>
-      <TouchableOpacity
-        onPress={()=>navigation.navigate('Signup')}>
-        <Text style={{fontSize:20,padding:80,color:'blue'}}>Create an Account?</Text>
-      </TouchableOpacity>
-  
-  </View>
-  )
+    }
 }
 
 const styles = StyleSheet.create({
-  container:{
-   flex: 1,
-   color: '#b2beb5',
-   justifyContent: 'center',
-  },
-   inputContainer: {
-    borderBottomColor: 'blue',
-    backgroundColor: '#FFFFFF',
-    borderRadius:5,
-    borderBottomWidth: 1,
-    width:350,
-    height:45,
-    marginBottom:20,
-    flexDirection: 'row',
-    alignItems:'center'
-  },
+    mycard:{
+        margin:9,
+        padding:8,
+        position: 'relative'
+
+       
+    },
+    cardView:{
+         flexDirection:"row",
+         padding:6,
+         alignItems: 'center'
+         
+        
+    },
+    text:{
+        fontSize:18,
+    },
+    button: {
+      position: 'relative',
+      margin: 16,
+     // right: 0,
+      //bottom: 50,
+      //top:50,
+      
+      
+    },
+    
 })
 
+export default Home;
 
-
-  export default Home
